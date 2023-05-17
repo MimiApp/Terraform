78c7e207a040f80af9921e2083c6d8633a5d6f5f
emidinho <emidioalemnju@yahoo.com>
2023-05-17 11:06:28 +0800

    update

--- 13-S3 Backend & Dynamodb Lock/backend.tf
+++ 13-S3 Backend & Dynamodb Lock/backend.tf
@@ -1,7 +1,7 @@
 # Below is Backend block
 terraform {
   backend "s3" {
-    bucket         = "emidiobuckt"
+    bucket         = "emidiobucket"
     key            = "backend/dev/terraform.tfstate"
     region         = "us-east-1"
     profile        = "default"
